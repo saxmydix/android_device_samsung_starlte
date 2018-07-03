de2d2217cb96e758a71883bdcff6c5662ff89ec7
saxmydix <linexsg@gmail.com>
2018-07-03 03:00:06 +0000

    fix some error
    Change-Id: I0e09cbf82be76f8e6a3c78b9bac6ec3ffc654e13

--- device.mk
+++ device.mk
@@ -1,7 +1,10 @@
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage 
 
 # Property overrides
 -include $(LOCAL_PATH)/system_prop.mk
